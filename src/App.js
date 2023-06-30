333c094401b592b776d30e09dfe183bed11b5740
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-06-30 00:40:06 -0400

    NavBar design complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Navbar from './components/NavBar/Navbar';
 import Loginpage from './components/loginpage/Loginpage';
 import Signup from './components/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Loginpage/>
+   <Navbar/>
     </div>
   );
 }
--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div>
-      
+    
+    <div className='navbar'>
+      <img className='navbarimage' src={require('/home/binil/Development/code/phase-2/pjct-phase-2/trailertube/src/images/TrailerTube.jpg')}  alt='noooooooooo'  />
+       <div className="buttonset">
+        <button className='button'>Popular</button>
+       <button className='button'>Top Rated Movies </button>
+       <button className='button'>Trending</button>
+       <button className='button'>Upcoming</button>
+       </div>
     </div>
+
+
+    
   )
 }
 
