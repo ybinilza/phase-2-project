e22f74f306deaa78f0e59758aebfd40ad06457cc
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-07-01 20:33:29 -0400

    Components Updated

--- src/components/Moviesaddedbyuser/Usermoviedisplay.js
+++ src/components/Moviesaddedbyuser/Usermoviedisplay.js
@@ -16,19 +16,4 @@ function Usermoviedisplay({movies}) {
 
 export default Usermoviedisplay
 
-
-
-/*
-{ moviedata && moviedata.map((obj) =>
-        {
-           return(<div>
-            <h3 className='movietitle'>Hello</h3>
-             <img  onClick={()=>handleClick()} className='poster'   src="https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w"  alt="Missing Image" />
-          </div>)
-
-        })
-       
-   }
-    
-
 */
